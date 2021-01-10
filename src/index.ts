01bcb028f3cb8b14c5216dc92deac574266d0f4d
Ryan James <rcjames1004@gmail.com>
2021-01-10 10:38:49 -0800

    Use typescript to build

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import CatheterPathServiceProtos from "../lib/catheter_path_service_pb"
+import CommonProtos from "../lib/common_pb"
+import DeviceServiceProtos from "../lib/device_service_pb"
+import SessionServiceProtos from "../lib/session_service_pb"
+import StateManagerServiceProtos from "../lib/state_manager_pb"
+
+export {
+    CatheterPathServiceProtos,
+    CommonProtos,
+    DeviceServiceProtos,
+    SessionServiceProtos,
+    StateManagerServiceProtos,
+}
